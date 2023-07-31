88eb4054018c1bec55599512814ced06891b5e29
Guilherme Paes <paes.guilherme@hotmail.com>
2023-07-31 20:41:48 -0300

    home screen style finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
 import React from "react";
 import styles from "./App.module.scss";
 import { ReactComponent as ImageSample } from "./assets/image.svg";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className={styles.App}>
-      <div className={styles.container}>
-        <h1 className={styles.title}>Upload your image</h1>
-        <p className={styles.subtitle}>File should be Jpeg, Png,...</p>
-        <div className={styles.uploader}>
-          <ImageSample />
-          <h2 className={styles.subtitle}>Drag & Drop your image here</h2>
+    <>
+      <div className={styles.App}>
+        <div className={styles.container}>
+          <h1 className={styles.container__title}>Upload your image</h1>
+          <p className={styles.container__subtitle}>File should be Jpeg, Png,...</p>
+          <div className={styles.dropContainer}>
+            <ImageSample className={styles.dropContainer__img} />
+            <h2 className={styles.dropContainer__txt}>Drag & Drop your image here</h2>
+          </div>
+          <p className={styles.dropContainer__txt}>Or</p>
+          <button className={styles.uploadButton}>Choose a file</button>
         </div>
-        <button className={styles.uploadButton}>Choose a file</button>
       </div>
-      <footer>Created by @guipaex - devChallenges.io</footer>
-    </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Footer.module.scss";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      Created by{" "}
+      <a className={styles.footer__link} href='https://github.com/guipaex' target='_blank'>
+        @guipaex
+      </a>
+      - devChallenges.io
+    </footer>
+  );
+}
